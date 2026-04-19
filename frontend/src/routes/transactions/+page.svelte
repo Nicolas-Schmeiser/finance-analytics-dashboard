e359882eb5a42d5a9d8baa2fe37c9a07a3977f50
Nicolas Bury <nicolas.bury@outlook.com>
2026-04-19 12:01:31 +0200

    Create transaction page route

---
+++ frontend/src/routes/transactions/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<h1>Transactions</h1>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Category</th>
+        </tr>
+    </thead>
+
+    <tbody>
+    </tbody>
+</table>
